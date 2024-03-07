eda42d1295efc6f84bfb709b671cd6bce57507b7
LeeHanByeol <byol0425@gmail.com>
2024-03-08 00:05:02 +0900

    initial files

---
+++ src/main/java/com/RunningMap/RunningMapApplication.java
@@ -0,0 +1,13 @@
+package com.RunningMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RunningMap/RunningMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RunningMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
